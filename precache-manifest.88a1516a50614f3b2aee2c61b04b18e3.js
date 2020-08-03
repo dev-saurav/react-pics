d0bf20a7b23300a69acdb3078dc4d1978ea6a16b
webDevSaurav <webdevelopersaurav@gmail.com>
2020-08-03 16:31:15 +0530

    Updates

---
+++ precache-manifest.88a1516a50614f3b2aee2c61b04b18e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3bac59d7a252ae3ba69d934ff8c85d",
+    "url": "/react-pics/index.html"
+  },
+  {
+    "revision": "eed1809de0b00f0d1387",
+    "url": "/react-pics/static/css/main.98c5978e.chunk.css"
+  },
+  {
+    "revision": "18016f3c683fb5abffbe",
+    "url": "/react-pics/static/js/2.5f8ad14d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pics/static/js/2.5f8ad14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed1809de0b00f0d1387",
+    "url": "/react-pics/static/js/main.dcbbf078.chunk.js"
+  },
+  {
+    "revision": "f93e97bad7c76fe80bf7",
+    "url": "/react-pics/static/js/runtime-main.2abc479c.js"
+  }
+]);
